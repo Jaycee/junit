b98f5345278c257f5c10ad6e87bf460e9f94ad81
Jayce <heaven23@126.com>
2022-04-30 20:35:32 +0800

    Initial commit

---
+++ src/tst/junittset.java
@@ -0,0 +1,19 @@
+package tst;
+
+import org.junit.Test;
+
+/**
+ * @Author: jayce
+ * @Date: 2022/4/9
+ * @Description: tst
+ * @time: 16:40
+ */
+public class junittset {
+    @Test
+    public void teas(){
+        String s1 = "sad";
+        String s2 = "happy";
+        System.out.println(s1.equals(s2));
+
+    }
+}
